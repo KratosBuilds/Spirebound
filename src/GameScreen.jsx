1de47f51a6f48a44a626a7728dd25798074c2ebe
Ahsan <ahsan.strength@gmail.com>
2025-08-17 21:59:17 -0400

    Add GameScreen component to display animated collectibles.

---
+++ src/GameScreen.jsx
@@ -0,0 +1,14 @@
+// Contents of src/GameScreen.jsx
+
+import React from 'react';
+
+const GameScreen = () => {
+    return (
+        <div>
+            <h1>Game Screen</h1>
+            {/* Animated collectibles will be displayed here */}
+        </div>
+    );
+};
+
+export default GameScreen;
