260c623fd2b51f8fb2044b6391ee987169460424
Ahsan <ahsan.strength@gmail.com>
2025-08-18 15:02:07 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,77 +1,77 @@
-const canvas = document.getElementById('gameCanvas');
-const ctx = canvas.getContext('2d');
+window.onload = function() {
+  const canvas = document.getElementById('gameCanvas');
+  const ctx = canvas.getContext('2d');
 
-// Load hero sprite
-const spriteImg = new Image();
-spriteImg.src = 'hero.png'; // Make sure the name matches exactly!
+  // Load hero sprite
+  const spriteImg = new Image();
+  spriteImg.src = 'hero.png'; // Make sure it's exactly this name
 
-function drawBackground() {
-  // Sky
-  ctx.fillStyle = '#222';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  spriteImg.onload = function() {
+    drawBackground();
+    drawHero();
+  };
 
-  // Mountains (three layers for effect)
-  // Back mountain
-  ctx.beginPath();
-  ctx.moveTo(0, 350);
-  ctx.lineTo(100, 200);
-  ctx.lineTo(250, 350);
-  ctx.lineTo(400, 350);
-  ctx.lineTo(400, 400);
-  ctx.lineTo(0, 400);
-  ctx.closePath();
-  ctx.fillStyle = '#444';
-  ctx.fill();
+  function drawBackground() {
+    // Sky
+    ctx.fillStyle = '#222';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  // Middle mountain
-  ctx.beginPath();
-  ctx.moveTo(50, 350);
-  ctx.lineTo(180, 240);
-  ctx.lineTo(320, 350);
-  ctx.lineTo(400, 350);
-  ctx.lineTo(400, 400);
-  ctx.lineTo(0, 400);
-  ctx.closePath();
-  ctx.fillStyle = '#666';
-  ctx.fill();
+    // Mountains (three layers for effect)
+    // Back mountain
+    ctx.beginPath();
+    ctx.moveTo(0, 350);
+    ctx.lineTo(100, 200);
+    ctx.lineTo(250, 350);
+    ctx.lineTo(400, 350);
+    ctx.lineTo(400, 400);
+    ctx.lineTo(0, 400);
+    ctx.closePath();
+    ctx.fillStyle = '#444';
+    ctx.fill();
 
-  // Front mountain
-  ctx.beginPath();
-  ctx.moveTo(120, 350);
-  ctx.lineTo(200, 280);
-  ctx.lineTo(280, 350);
-  ctx.lineTo(400, 350);
-  ctx.lineTo(400, 400);
-  ctx.lineTo(0, 400);
-  ctx.closePath();
-  ctx.fillStyle = '#888';
-  ctx.fill();
+    // Middle mountain
+    ctx.beginPath();
+    ctx.moveTo(50, 350);
+    ctx.lineTo(180, 240);
+    ctx.lineTo(320, 350);
+    ctx.lineTo(400, 350);
+    ctx.lineTo(400, 400);
+    ctx.lineTo(0, 400);
+    ctx.closePath();
+    ctx.fillStyle = '#666';
+    ctx.fill();
 
-  // Moon
-  ctx.beginPath();
-  ctx.arc(320, 80, 40, 0, Math.PI * 2);
-  ctx.fillStyle = '#bbb';
-  ctx.fill();
+    // Front mountain
+    ctx.beginPath();
+    ctx.moveTo(120, 350);
+    ctx.lineTo(200, 280);
+    ctx.lineTo(280, 350);
+    ctx.lineTo(400, 350);
+    ctx.lineTo(400, 400);
+    ctx.lineTo(0, 400);
+    ctx.closePath();
+    ctx.fillStyle = '#888';
+    ctx.fill();
 
-  // Stars
-  for (let i = 0; i < 40; i++) {
-    let x = Math.random() * canvas.width;
-    let y = Math.random() * 200;
-    ctx.fillStyle = '#fff';
-    ctx.fillRect(x, y, 2, 2);
+    // Moon
+    ctx.beginPath();
+    ctx.arc(320, 80, 40, 0, Math.PI * 2);
+    ctx.fillStyle = '#bbb';
+    ctx.fill();
+
+    // Stars
+    for (let i = 0; i < 40; i++) {
+      let x = Math.random() * canvas.width;
+      let y = Math.random() * 200;
+      ctx.fillStyle = '#fff';
+      ctx.fillRect(x, y, 2, 2);
+    }
   }
-}
 
-function drawHero() {
-  // Center the hero horizontally, place on ground
-  const x = canvas.width / 2 - 56;
-  const y = canvas.height - 112;
-  ctx.drawImage(spriteImg, x, y, 112, 112);
-}
-
-function render() {
-  drawBackground();
-  drawHero();
-}
-
-spriteImg.onload = render;
+  function drawHero() {
+    // Center the hero horizontally, place on ground
+    const x = canvas.width / 2 - 56;
+    const y = canvas.height - 112;
+    ctx.drawImage(spriteImg, x, y, 112, 112);
+  }
+};
